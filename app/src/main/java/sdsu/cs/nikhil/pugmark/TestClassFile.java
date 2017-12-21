2f5779689e8ecf46dcde774820742ce2ecdd8f42
Nikhil Chandrashekar <nikhil0689@gmail.com>
2017-12-21 16:50:06 -0500

    test commit

--- app/src/main/java/sdsu/cs/nikhil/pugmark/TestClassFile.java
+++ app/src/main/java/sdsu/cs/nikhil/pugmark/TestClassFile.java
@@ -1,8 +1,13 @@
 package sdsu.cs.nikhil.pugmark;
 
+import android.util.Log;
+
 /**
  * Created by nikhilc on 12/21/2017.
  */
 
 public class TestClassFile {
+    int i = 0;
+    //Initial Commit
+
 }
